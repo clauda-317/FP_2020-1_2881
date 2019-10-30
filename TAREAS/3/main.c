71f625fb4f7e6adc6c98205da7cbd0f3feffeb9e
clauda-317 <54477432+clauda-317@users.noreply.github.com>
2019-10-29 22:27:19 -0600

    Adjunto tarea 3

---
+++ TAREAS/3/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	float a, b, c;
+	scanf("%f", &a);
+	scanf("%f", &b);
+	
+		c=a+b;
+		printf("%.2f\n", c);
+		c=a-b;
+		printf("%.2f\n", c);
+		c=a*b;
+		printf("%.2f\n",c);
+		c=a/b;
+		printf("%.2f\n",c);	
+
+	return 0;
+}
+
